027e74ce3355901ceba2e215708ce0885c0c44dc
santiagobedoa <santiagobedoa@gmail.com>
2023-01-24 15:49:27 -0500

    Tue Jan 24 15:49:27 -05 2023

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""4. Task"""
+import asyncio
+from typing import List
+
+task_wait_random = __import__("3-tasks").task_wait_random
+
+
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """
+    return the list of all the delays (float values).
+    The list of the delays should be in ascending order without using sort()
+    because of concurrency.
+    """
+    results = []
+    promises = []
+    for _ in range(0, n):
+        task = asyncio.create_task(task_wait_random(max_delay))
+        task.add_done_callback(lambda x: results.append(x.result()))
+        promises.append(task)
+
+    for promise in promises:
+        await promise
+
+    return results
