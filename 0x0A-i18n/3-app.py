0e4eec04ea00d94368df22a94243f868aa1f0cc9
santiagobedoa <santiagobedoa@gmail.com>
2023-02-23 19:45:54 -0500

    Thu Feb 23 19:45:54 -05 2023

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """ a basic flask app"""
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 app = Flask(__name__)
 
 
 class Config(object):
-    """ Config class for Babel object """
+    """Config class for Babel object"""
+
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -17,17 +18,17 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-@app.route('/')
+@app.route("/")
 def hello():
-    """ render a basic html file """
-    return render_template('3-index.html')
+    """render a basic html file"""
+    return render_template("3-index.html")
 
 
 @babel.localeselector
 def get_locale():
-    """ a function to determine the best match with the supported languages """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    """a function to determine the best match with the supported languages"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
