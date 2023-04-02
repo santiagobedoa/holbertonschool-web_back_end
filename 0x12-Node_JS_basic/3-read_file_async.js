9c6692809b03663e0da53933c5706f4186db6fcc
santiagobedoa <santiagobedoa@gmail.com>
2023-04-02 13:38:58 -0500

    Sun Apr  2 13:38:57 -05 2023

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -1,1 +1,44 @@
+const fs = require("fs");
 
+const countStudents = (aPath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(aPath, "utf8", (err, data) => {
+      if (err) {
+        reject(new Error("Cannot load the database"));
+        return;
+      }
+
+      const lines = data.trim().split("\n");
+      const headers = lines[0].split(",");
+      const students = lines
+        .slice(1)
+        .filter((line) => line.trim() !== "")
+        .map((line) => {
+          const values = line.split(",");
+          return headers.reduce((obj, header, i) => {
+            obj[header] = values[i];
+            return obj;
+          }, {});
+        });
+
+      const courses = {};
+      students.forEach((student) => {
+        const course = student["field"];
+        if (!courses[course]) courses[course] = [];
+        courses[course].push(student["firstname"]);
+      });
+
+      console.log(`Number of students: ${students.length}`);
+      for (const course in courses) {
+        console.log(
+          `Number of students in ${course}: ${
+            courses[course].length
+          }. List: ${courses[course].join(", ")}`
+        );
+      }
+      resolve();
+    });
+  });
+};
+
+module.exports = countStudents;
